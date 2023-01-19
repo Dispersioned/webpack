9a84514036dca9dbbb619ddb75b569157a73d443
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-01-19 20:40:35 +0300

    feat: cleanup & style reset

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,5 @@
-// import Img from './assets/favicon.png';
+import './styles/global.css';
 
 export function App() {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nesciunt, quam voluptatem laborum ducimus
-        asperiores facilis temporibus harum provident quia.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus delectus ab perferendis, iste non
-        blanditiis qui, cum 2uo rem fugit ipsum. Vitae, quod.
-      </p>
-      <img
-        src={undefined}
-        alt=""
-        style={{
-          width: 300,
-          height: 300,
-          display: 'block',
-        }}
-      />
-    </div>
-  );
+  return <div>app</div>;
 }
