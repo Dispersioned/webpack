0edf70a27c5901b8e778f941c8a4b134e42a0b63
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-09-04 15:54:22 +0300

    Update assets.d.ts

--- assets.d.ts
+++ assets.d.ts
@@ -7,3 +7,16 @@ declare module '*.ttf';
 declare module '*.woff';
 declare module '*.woff2';
 declare module '*.eot';
+declare module '*.scss' {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
